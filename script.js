d5cb8a62ee6abf438789673fa4d41f1a91d9cd45
matheusmb41 <matheusmenezesb@hotmail.com>
2024-11-01 17:08:42 -0300

    Atualização
    Criação de rede social e ajustes gerais

--- script.js
+++ script.js
@@ -16,40 +16,15 @@ $(document).ready(function () {
 
   // Rolagem suave
   $("nav ul li a").click(function (e) {
-    e.preventDefault(); // Impede o comportamento padrão do link
-
-    var target = $(this).attr("href"); // Obtém o valor do atributo href (ex: #servicos)
-    $("html, body").animate(
-      {
-        scrollTop: $(target).offset().top, // Anima a rolagem até a posição da seção alvo
-      },
-      800 // Duração da animação em milissegundos
-    );
+    const target = $(this).attr("href");
+    if (!target.startsWith("http")) {
+      e.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: $(target).offset().top,
+        },
+        800
+      );
+    }
   });
-});
-
-//Scroll animado
-function elementInViewport(el) {
-  const rect = el.getBoundingClientRect();
-  const windowHeight =
-    window.innerHeight || document.documentElement.clientHeight;
-  const windowWidth = window.innerWidth || document.documentElement.clientWidth;
-
-  // Verifica se a div está parcialmente visível
-  return (
-    rect.top <= windowHeight * 0.8 && // Ativa antes de chegar ao topo completo
-    rect.bottom >= windowHeight * 0.2 // E permanece até 20% do final
-  );
-}
-document.addEventListener("DOMContentLoaded", function () {
-  function handleScrollAnimation() {
-    document.querySelectorAll(".scroll-animation").forEach((el) => {
-      if (elementInViewport(el)) {
-        el.classList.add("active");
-      }
-    });
-  }
-
-  window.addEventListener("scroll", handleScrollAnimation);
-  handleScrollAnimation();
 });
